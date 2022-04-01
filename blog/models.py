61f1b41fad28bc071c5a82e684a256b10bbe581a
HSFantast1k <bohdankoval3012@gmail.com>
2022-04-01 20:15:37 +0300

    pegination

--- blog/models.py
+++ blog/models.py
@@ -63,15 +63,21 @@ class Post(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Опубликоваано')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name    ='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',
+                              blank=True,
+                              verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
     category = models.ForeignKey(Category,
                                  on_delete=models.PROTECT,
-                                 related_name='posts', verbose_name='Категория')
+                                 related_name='posts',
+                                 verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug': self.slug})
 
     class Meta:
         ordering = ['-created_at']
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='home'), 
+    path('', Home.as_view(), name='home'), 
     path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', get_post, name='post'),
 ]
