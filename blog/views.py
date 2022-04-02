307a6bd58246507f727e91f7fd330ad616bca003
HSFantast1k <bohdankoval3012@gmail.com>
2022-04-02 22:09:30 +0300

    single page post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from pyexpat import model
 from unicodedata import category
 from django.shortcuts import render
-from django.views.generic import ListView, DateDetailView
+from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 
 class Home(ListView):
@@ -30,15 +31,19 @@ class PostsByCategory(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = Category.objects.get(slug=self.kwargs['slug'])
         return context
+    
+class PostsByTag(ListView):
+    pass
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
-
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
-
-
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
